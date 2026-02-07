a90057c7532a8cdd18f92627a7e076380855f5f3
tlima1011 <thiagolima1011@gmail.com>
2026-02-07 20:26:28 -0300

    Classe Student Configurada toString()

--- src/com/rent/hotel/com/model/Student.java
+++ src/com/rent/hotel/com/model/Student.java
@@ -1,5 +1,13 @@
 package com.rent.hotel.com.model;
 
 public class Student {
-
+	
+	private String Name; 
+	private String Email;
+	
+	
+	@Override
+	public String toString() {
+		return Name + ", " + Email;
+	} 
 }
