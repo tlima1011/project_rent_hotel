4e1b2a38dacc239a5906f515f4a610fdfed8f6e9
tlima1011 <thiagolima1011@gmail.com>
2026-02-07 23:45:10 -0300

    Saída de Vetores para Classe Student

--- src/com/rent/hotel/com/main/Main.java
+++ src/com/rent/hotel/com/main/Main.java
@@ -8,41 +8,41 @@ import com.rent.hotel.com.model.Student;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Locale.setDefault(Locale.US); 
-		Scanner ler = new Scanner(System.in); 
-		
-		Student [] rooms = new Student[10]; 
-		
+
+		Locale.setDefault(Locale.US);
+		Scanner ler = new Scanner(System.in);
+
+		Student[] rooms = new Student[10];
+
 		System.out.print("Quantos quartos serão alugados? ");
-		int n = ler.nextInt(); 
-		ler.nextLine(); 
-		
-		for(int i = 0; i < n;i++) {
-			System.out.printf("Aluguel #%d\n", (i+1));
+		int n = ler.nextInt();
+		ler.nextLine();
+
+		for (int i = 0; i < n; i++) {
+			System.out.printf("Aluguel #%d\n", (i + 1));
 			System.out.print("Nome: ");
 			String name = ler.nextLine();
 			System.out.print("Email: ");
 			String email = ler.nextLine();
 			System.out.print("Quarto: ");
 			int quarto = ler.nextInt();
-			ler.nextLine(); 
-			if (rooms[i] == null)
-			{
-			    rooms[quarto] = new Student(name,email);
+			ler.nextLine();
+			// while(rooms[quarto] != null) {
+			if (rooms[quarto] != null) {
+				System.out.println("Quarto já ocupado");
+			} else {
+				rooms[quarto] = new Student(name, email);
 			}
+			// }
+//			if (rooms[quarto] == null)
+//			{
+//			    
+//			}
 			System.out.println();
 		}
 		
-		System.out.println("\nQuartos Ocupados");
-		for(int i = 0; i < rooms.length; i++)
-		{
-		    if (rooms[i] != null)
-		    {
-		    		System.out.println(i + ": "+rooms[i]);
-		    }
-		}
-			
+		Student s = new Student(); 		
+		s.relatorio(rooms); 
 		ler.close();
 	}
 }
--- src/com/rent/hotel/com/model/Student.java
+++ src/com/rent/hotel/com/model/Student.java
@@ -5,13 +5,24 @@ public class Student {
 	private String Name; 
 	private String Email;
 	
+	public Student() {
+		
+	}
 	
 	public Student(String n, String e) {
 		Name = n; 
 		Email = e; 
 	}
 	
-	
+	public void relatorio(Student [] rooms) {
+		System.out.println("\nQuartos Ocupados");
+		for (int i = 0; i < rooms.length; i++) {
+			if (rooms[i] != null) {
+				System.out.println(i + ": " + rooms[i]);
+			}
+		}
+	}
+
 	@Override
 	public String toString() {
 		return Name + ", " + Email;
